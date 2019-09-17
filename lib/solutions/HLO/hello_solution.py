6e738ebfd5a6000d53d30803a2138d390138e320
sbx_user1051 <sbx_user1051@169.254.235.117>
2019-09-17 20:49:07 +0000

    Tue Sep 17 20:49:07 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,8 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    if type(friend_name) != str:
-        raise TypeError("Expected a string")
     return "Hello World"
+
 
 
